d6736c9717dac055b312fd0bbdef178a1e6a0c30
Kacper <kacperkowalski12345678@gmail.com>
2020-11-18 12:00:49 +0100

    change

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ const navBtn = document.querySelector(".fa-bars");
 const navLink = document.querySelector(".nav__mobile");
 
 
-const test = () =>{
+const menu = () =>{
     navLink.classList.toggle("nav__mobile-active")
 }
 
-navBtn.addEventListener('click', test)
+navBtn.addEventListener('click', menu)
